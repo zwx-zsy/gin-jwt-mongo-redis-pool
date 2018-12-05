0f8da70def483943980ffe63f21645035fb18d32
yancyzhou <python@vip.126.com>
2018-12-05 11:31:57 +0800

    update

--- App/Login.go
+++ App/Login.go
@@ -18,65 +18,64 @@ type LoginParam struct {
 	Password string `form:"password" json:"password" binding:"required"`
 }
 
-
 //登录操作
 func Login(c *gin.Context) {
 	var jsons LoginParam
 	if err := c.ShouldBindJSON(&jsons); err == nil {
 		result := M.Person{}
 		err := M.Persons(c).Find(bson.M{"Name": jsons.User}).One(&result)
-		if err != nil{
+		if err != nil {
 			switch err {
 			case mgo.ErrNotFound:
 				c.AbortWithStatusJSON(http.StatusOK, gin.H{
 					"code": http.StatusNotFound,
-					"msg": err.Error(),
+					"msg":  err.Error(),
 				})
 			default:
 
 				c.JSON(http.StatusOK, gin.H{
 					"code": http.StatusInternalServerError,
-					"msg": err.Error(),
+					"msg":  err.Error(),
 				})
 			}
-		}else {
-			if jsons.User ==  result.Name&& jsons.Password == result.PassWord {
+		} else {
+			if jsons.User == result.Name && jsons.Password == result.PassWord {
 				generateToken(c, jsons.User)
 			} else {
 				c.JSON(http.StatusOK, gin.H{
 					"code": 400,
-					"msg":    "用户名或密码错误！",
+					"msg":  "用户名或密码错误！",
 				})
 			}
 		}
 
-
 	} else {
-		c.JSON(http.StatusBadRequest, gin.H{"code":402,"msg":err.Error()})
+		c.JSON(http.StatusBadRequest, gin.H{"code": 402, "msg": err.Error()})
 	}
 }
-
 
 func generateToken(c *gin.Context, user string) {
 	j := &Lib.JWT{
 		[]byte("www.vcoding.com"),
 	}
 
+	value, _ := c.Get(Lib.CONFKEY)
+
 	claims := Lib.CustomClaims{
-		Payload:Lib.Payload{
+		Payload: Lib.Payload{
 			user,
-			true},
-		StandardClaims:Jwtgo.StandardClaims{
-			NotBefore: int64(time.Now().Unix() - 1000), // 签名生效时间
-			ExpiresAt: int64(time.Now().Unix() + 3600), // 过期时间 一小时
-			Issuer:    "newtrekWang",                   //签名的发行者
-			},
+			false},
+		StandardClaims: Jwtgo.StandardClaims{
+			NotBefore: int64(time.Now().Unix() + value.(*Lib.Yaml).JwtConf.Notbefore), // 签名生效时间
+			ExpiresAt: int64(time.Now().Unix() + value.(*Lib.Yaml).JwtConf.Exptime),   // 过期时间 一小时
+			Issuer:    value.(*Lib.Yaml).JwtConf.Issuer,                               //签名的发行者
+		},
 	}
 
 	if token, err := j.CreateToken(claims); err != nil {
 		c.JSON(http.StatusOK, gin.H{
 			"code": 500,
-			"msg":    err.Error(),
+			"msg":  err.Error(),
 		})
 		return
 	} else {
--- Lib/Base.go
+++ Lib/Base.go
@@ -1,22 +1,34 @@
 package Lib
 
 const (
-	CONFPATH string = "/septnet/config/conf.yaml"
-	CONFKEY string = "Config"
+	CONFPATH string = "/septnet/config/conf.yaml" // 配置文件地址
+	CONFKEY  string = "Config"                    //配置文件的key
 )
 
+//yaml的结构
+
+//start
 type Yaml struct {
-	ConfigKey string `yaml:"ConfigKey"`
-	DBConf MongoDB `yaml:"mongodb"`
-	RedisConf Redis `yaml:"redis"`
-	RedisConn string `yaml:"redisConn"`
+	ConfigKey string  `yaml:"ConfigKey"`
+	DBConf    MongoDB `yaml:"mongodb"`
+	RedisConf Redis   `yaml:"redis"`
+	RedisConn string  `yaml:"redisConn"`
+	JwtConf   JwtConf `yaml:"JwtConf"`
 }
 
 type MongoDB struct {
-	User string `yaml:"db_user"`
-	Host string `yaml:"db_host"`
-	Password string `yaml:"db_pass"`
-	Port string `yaml:"db_port"`
+	User         string `yaml:"db_user"`
+	Host         string `yaml:"db_host"`
+	Password     string `yaml:"db_pass"`
+	Port         string `yaml:"db_port"`
 	DatabaseName string `yaml:"db_database_name"`
-	Uri string `yaml:"url"`
+	Uri          string `yaml:"url"`
 }
+
+type JwtConf struct {
+	Issuer    string `yaml:"issuer"`
+	Exptime   int64  `yaml:"exptime"`
+	Notbefore int64  `yaml:"notbefore"`
+}
+
+//end
