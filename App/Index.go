0528a173ce1008569b800b02b5cefabc2c0f7341
yancyzhou <python@vip.126.com>
2019-01-22 15:24:35 +0800

    format

--- App/Index.go
+++ App/Index.go
@@ -1,138 +1,138 @@
 package Api
 
 import (
-    "TimeLine/Lib"
-    M "TimeLine/Model"
-    "fmt"
-    "github.com/gin-gonic/gin"
-    "gopkg.in/mgo.v2/bson"
-    "net/http"
-    "strconv"
-    "time"
+	"TimeLine/Lib"
+	M "TimeLine/Model"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"gopkg.in/mgo.v2/bson"
 )
 
 // 登录参数
 type PersonParam struct {
-    NickName string `form: "nickname" json: "nickname" binding: "required"`
-    Sex      int    `form: "sex" json: "sex" binding: "-"`
-    Birthday string `form: "birthday" json: "birthday" binding: "required"`
-    Born     int    `form: "born" json: "born" binding: "-"`
-    Role     int    `form: "role" json: "role" binding: "-"`
+	NickName string `form: "nickname" json: "nickname" binding: "required"`
+	Sex      int    `form: "sex" json: "sex" binding: "-"`
+	Birthday string `form: "birthday" json: "birthday" binding: "required"`
+	Born     int    `form: "born" json: "born" binding: "-"`
+	Role     int    `form: "role" json: "role" binding: "-"`
 }
 
-func GetGrowthStandards(c * gin.Context) {
+func GetGrowthStandards(c *gin.Context) {
 	// 获取成长标准说明
-    skip, _: = strconv.Atoi(c.Param("skip"))
-    limit, _: = strconv.Atoi(c.Param("limit"))
-    gs: = []M.GrowthStandard{}
-    e: = M.GrowthStandards().Find(nil).Sort("Type", "Days").Skip(skip).Limit(limit).All( & gs)
+	skip, _ := strconv.Atoi(c.Param("skip"))
+	limit, _ := strconv.Atoi(c.Param("limit"))
+	gs := []M.GrowthStandard{}
+	e := M.GrowthStandards().Find(nil).Sort("Type", "Days").Skip(skip).Limit(limit).All(&gs)
 
-    if e != nil {
-            c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError,
-                                                                        "msg": http.StatusText(http.StatusInternalServerError)})
-    } else {
-            c.JSON(http.StatusOK, gin.H{"code": http.StatusOK, "msg": "success", "data": gs, "Total": len(gs)})
-    }
+	if e != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError,
+			"msg": http.StatusText(http.StatusInternalServerError)})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"code": http.StatusOK, "msg": "success", "data": gs, "Total": len(gs)})
+	}
 }
 
-func CreatePerson(ctx * gin.Context) {
-    claims, _: = Lib.GetPayLoad(ctx)
-    var PersonP PersonParam
-    if err: = ctx.ShouldBindJSON( & PersonP); err == nil {
-        result: = M.Person{Id: bson.NewObjectId(), NickName: PersonP.NickName,
-                           Sex: PersonP.Sex, Birthday: PersonP.Birthday, Born: PersonP.Born, Role: PersonP.Role, OpenId: claims.Payload.OpenId, CreateDateTime: time.Now()}
-        err: = M.Persons().Insert(& result)
-        if err != nil {
-            ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"code": http.StatusPaymentRequired, "msg": err.Error()})
-        } else {
-            userone: = &M.User{}
-            user: = M.User{WxOpenId: claims.Payload.OpenId, PersonId: result.Id.Hex()}
-            err: = M.Users().Find(bson.M{"WxOpenId": claims.Payload.OpenId}).One( & userone)
-            if err != nil {
-                user.CreateDateTime = time.Now()
-            }
-            ups, err: = M.Users().Upsert(bson.M{"WxOpenId": claims.Payload.OpenId}, user)
-            if err != nil {
-                defer M.Rollback(M.CollectionName_Person, result.Id)
-                ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError, "msg": err.Error()})
-            } else {
-                ctx.JSON(http.StatusOK, gin.H{"code": http.StatusOK, "msg": "success", "data": ups.UpsertedId})
-            }
-        }
-    } else {
-        ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"code": http.StatusPaymentRequired, "msg": err.Error()})
-    }
+func CreatePerson(ctx *gin.Context) {
+	claims, _ := Lib.GetPayLoad(ctx)
+	var PersonP PersonParam
+	if err := ctx.ShouldBindJSON(&PersonP); err == nil {
+		result := M.Person{Id: bson.NewObjectId(), NickName: PersonP.NickName,
+			Sex: PersonP.Sex, Birthday: PersonP.Birthday, Born: PersonP.Born, Role: PersonP.Role, OpenId: claims.Payload.OpenId, CreateDateTime: time.Now()}
+		err := M.Persons().Insert(&result)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"code": http.StatusPaymentRequired, "msg": err.Error()})
+		} else {
+			userone := &M.User{}
+			user := M.User{WxOpenId: claims.Payload.OpenId, PersonId: result.Id.Hex()}
+			err := M.Users().Find(bson.M{"WxOpenId": claims.Payload.OpenId}).One(&userone)
+			if err != nil {
+				user.CreateDateTime = time.Now()
+			}
+			ups, err := M.Users().Upsert(bson.M{"WxOpenId": claims.Payload.OpenId}, user)
+			if err != nil {
+				defer M.Rollback(M.CollectionName_Person, result.Id)
+				ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError, "msg": err.Error()})
+			} else {
+				ctx.JSON(http.StatusOK, gin.H{"code": http.StatusOK, "msg": "success", "data": ups.UpsertedId})
+			}
+		}
+	} else {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"code": http.StatusPaymentRequired, "msg": err.Error()})
+	}
 }
 
 //获取用户信息
 
 type UserResult struct {
-    User M.Person
-    Days int
-    List[]M.Person
+	User M.Person
+	Days int
+	List []M.Person
 }
 type Users M.User
-func(t Users) String() string  {
-    return fmt.Sprintf("学号: %s\n真实姓名: %s\n年龄: %s\n", t.PersonId, t.WxOpenId, t.Id)
+
+func (t Users) String() string {
+	return fmt.Sprintf("学号: %s\n真实姓名: %s\n年龄: %s\n", t.PersonId, t.WxOpenId, t.Id)
 }
-func GetUserInfo(c * gin.Context) {
-    claims, err: = Lib.GetPayLoad(c)
-    if !err{
-        c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError, "msg": http.StatusText(http.StatusInternalServerError)})
-    }else {
-        userone: = &Users{PersonId: "sadasdfa", Id: "hsjkdfs"}
-        result: = &UserResult{}
-        fmt.Println(userone)
-        err: = M.Users().Find(bson.M{"WxOpenId": claims.Payload.OpenId}).One(& userone)
-        if err != nil {
-            if err.Error() == "not found" {
-                result.User = M.Person{}
-                result.Days = -1
-            } else {
-                c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError, "msg": http.StatusText(http.StatusInternalServerError)})
-            }
-        } else {
-            loc, _: = time.LoadLocation("Local")
-            if userone.PersonId != "" {
-                        err: = M.Persons().FindId(bson.ObjectIdHex(userone.PersonId)).One(& result.User)
-                if err != nil {
-                            c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError, "msg": http.StatusText(http.StatusInternalServerError)})
-                        }
-                toBeCharge: = result.User.Birthday + " 00:00:00"
-                parse_str_time, _: = time.ParseInLocation("2006-01-02 15:04:05", toBeCharge, loc)
-                result.Days = Lib.TimeSub(
-                            time.Now(), parse_str_time)
-            } else {
-                result.User = M.Person{}
-                result.Days = -1
+func GetUserInfo(c *gin.Context) {
+	claims, err := Lib.GetPayLoad(c)
+	if !err {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError, "msg": http.StatusText(http.StatusInternalServerError)})
+	} else {
+		userone := &Users{PersonId: "sadasdfa", Id: "hsjkdfs"}
+		result := &UserResult{}
+		fmt.Println(userone)
+		err := M.Users().Find(bson.M{"WxOpenId": claims.Payload.OpenId}).One(&userone)
+		if err != nil {
+			if err.Error() == "not found" {
+				result.User = M.Person{}
+				result.Days = -1
+			} else {
+				c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError, "msg": http.StatusText(http.StatusInternalServerError)})
+			}
+		} else {
+			loc, _ := time.LoadLocation("Local")
+			if userone.PersonId != "" {
+				err := M.Persons().FindId(bson.ObjectIdHex(userone.PersonId)).One(&result.User)
+				if err != nil {
+					c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError, "msg": http.StatusText(http.StatusInternalServerError)})
+				}
+				toBeCharge := result.User.Birthday + " 00:00:00"
+				parse_str_time, _ := time.ParseInLocation("2006-01-02 15:04:05", toBeCharge, loc)
+				result.Days = Lib.TimeSub(
+					time.Now(), parse_str_time)
+			} else {
+				result.User = M.Person{}
+				result.Days = -1
 
-            }
-        }
-        fmt.Println(userone)
-        errs: = M.Persons().Find(bson.M{"OpenId": claims.Payload.OpenId}).All(& result.List)
-        if errs != nil {
-            c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError,
-                                                                        "msg": http.StatusText(http.StatusInternalServerError)})
-        } else {
-            if result.List == nil {
-                result.List = []M.Person{}
-            }
-            c.JSON(http.StatusOK, gin.H{"code": http.StatusOK, "msg": "success", "data": result, "Total": len(result.List)})
-        }
-    }
-
+			}
+		}
+		fmt.Println(userone)
+		errs := M.Persons().Find(bson.M{"OpenId": claims.Payload.OpenId}).All(&result.List)
+		if errs != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError,
+				"msg": http.StatusText(http.StatusInternalServerError)})
+		} else {
+			if result.List == nil {
+				result.List = []M.Person{}
+			}
+			c.JSON(http.StatusOK, gin.H{"code": http.StatusOK, "msg": "success", "data": result, "Total": len(result.List)})
+		}
+	}
 
 }
 
-
-func GetPersonList(c * gin.Context) {
-    claims, _: = Lib.GetPayLoad(c)
-    result: = []M.Person{}
-    err: = M.Persons().Find(bson.M{"OpenId": claims.Payload.OpenId}).All( & result)
-    if err != nil {
-        c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError,
-                                                                    "msg": http.StatusText(http.StatusInternalServerError)})
-    } else {
-        c.JSON(http.StatusOK, gin.H{"code": http.StatusOK, "msg": "success", "data": result, "Total": len(result)})
-    }
+func GetPersonList(c *gin.Context) {
+	claims, _ := Lib.GetPayLoad(c)
+	result := []M.Person{}
+	err := M.Persons().Find(bson.M{"OpenId": claims.Payload.OpenId}).All(&result)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"code": http.StatusInternalServerError,
+			"msg": http.StatusText(http.StatusInternalServerError)})
+	} else {
+		c.JSON(http.StatusOK, gin.H{"code": http.StatusOK, "msg": "success", "data": result, "Total": len(result)})
+	}
 }
--- App/Login.go
+++ App/Login.go
@@ -1,164 +1,168 @@
 package Api
 
 import (
-    "TimeLine/Lib"
-    M "TimeLine/Model"
-    "encoding/json"
-    Jwtgo "github.com/dgrijalva/jwt-go"
-    "github.com/gin-gonic/gin"
-    "gopkg.in/mgo.v2"
-    "gopkg.in/mgo.v2/bson"
-    "io/ioutil"
-    "net/http"
-    "time"
+	"TimeLine/Lib"
+	M "TimeLine/Model"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"time"
+
+	Jwtgo "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 )
 
 // 登录参数
 type LoginParam struct {
-    User     string `form: "user" json: "user" binding: "-"`
-    Password string `form: "password" json: "password" binding: "required"`
+	User     string `form: "user" json: "user" binding: "-"`
+	Password string `form: "password" json: "password" binding: "required"`
 }
 
 //微信 code登录
 
 type WeixinCode struct {
-    Code string `form: "code" json: "code" binding: "required"`
+	Code string `form: "code" json: "code" binding: "required"`
 }
 
 //登录操作
-func Login(c * gin.Context) {
-    var jsons LoginParam
-    if err: = c.ShouldBindJSON(& jsons); err == nil {
-        result: = M.Person{}
-        err: = M.Persons().Find(bson.M{"Name": jsons.User}).One( & result)
-        if err != nil {
-            switch err {
-                case mgo.ErrNotFound:
-                c.AbortWithStatusJSON(http.StatusOK, gin.H{
-                    "code": http.StatusNotFound,
-                    "msg":  err.Error(),
-                })
-                default:
+func Login(c *gin.Context) {
 
-                c.JSON(http.StatusOK, gin.H{
-                    "code": http.StatusInternalServerError,
-                    "msg":  err.Error(),
-                })
-            }
-        } else {
-            if jsons.User == result.NickName & & jsons.Password == result.Birthday {
-                generateToken(c, Lib.Payload{Name: jsons.User})
-            } else {
-                c.JSON(http.StatusOK, gin.H{
-                    "code": http.StatusBadRequest,
-                    "msg":  "用户名或密码错误！",
-                })
-            }
-        }
+	var jsons LoginParam
+	if err := c.ShouldBindJSON(&jsons); err == nil {
+		result := M.Person{}
+		err := M.Persons().Find(bson.M{"Name": jsons.User}).One(&result)
+		if err != nil {
+			switch err {
+			case mgo.ErrNotFound:
+				c.AbortWithStatusJSON(http.StatusOK, gin.H{
+					"code": http.StatusNotFound,
+					"msg":  err.Error(),
+				})
+			default:
 
-    } else {
-        c.JSON(http.StatusBadRequest, gin.H{"code": http.StatusPaymentRequired, "msg": err.Error()})
-    }
+				c.JSON(http.StatusOK, gin.H{
+					"code": http.StatusInternalServerError,
+					"msg":  err.Error(),
+				})
+			}
+		} else {
+			if jsons.User == result.NickName && jsons.Password == result.Birthday {
+				generateToken(c, Lib.Payload{Name: jsons.User})
+			} else {
+				c.JSON(http.StatusOK, gin.H{
+					"code": http.StatusBadRequest,
+					"msg":  "用户名或密码错误！",
+				})
+			}
+		}
+
+	} else {
+		c.JSON(http.StatusBadRequest, gin.H{"code": http.StatusPaymentRequired, "msg": err.Error()})
+	}
 }
 
-func generateToken(c * gin.Context, payload Lib.Payload) string {
-    j: = &Lib.JWT{
-        []byte(Lib.SignKey),
-    }
-    claims: = Lib.CustomClaims{
-        Payload: payload,
-        StandardClaims: Jwtgo.StandardClaims{
-            NotBefore: int64(time.Now().Unix() + Lib.ServerConf.JwtConf.Notbefore), // 签名生效时间
-            ExpiresAt: int64(time.Now().Unix() + Lib.ServerConf.JwtConf.Exptime),   // 过期时间 一小时*24
-            Issuer:    Lib.ServerConf.JwtConf.Issuer,                               //签名的发行者
-        },
-    }
+func generateToken(c *gin.Context, payload Lib.Payload) string {
+	//set token
 
-    if token, err: = j.CreateToken(claims); err != nil {
-        c.JSON(http.StatusOK, gin.H{
-            "code": http.StatusInternalServerError,
-            "msg":  err.Error(),
-        })
-        return ""
-    } else {
-        // c.JSON(http.StatusOK, gin.H{
-            // "code": http.StatusOK,
-            // "msg":  http.StatusText(http.StatusOK),
-            // "data": token})
-        return token
-    }
+	customjwt := &Lib.JWT{
+		[]byte(Lib.SignKey),
+	}
+	claims := Lib.CustomClaims{
+		Payload: payload,
+		StandardClaims: Jwtgo.StandardClaims{
+			NotBefore: int64(time.Now().Unix() + Lib.ServerConf.JwtConf.Notbefore), // 签名生效时间
+			ExpiresAt: int64(time.Now().Unix() + Lib.ServerConf.JwtConf.Exptime),   // 过期时间 一小时*24
+			Issuer:    Lib.ServerConf.JwtConf.Issuer,                               //签名的发行者
+		},
+	}
+
+	if token, err := customjwt.CreateToken(claims); err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": http.StatusInternalServerError,
+			"msg":  err.Error(),
+		})
+		return ""
+	} else {
+		// c.JSON(http.StatusOK, gin.H{
+		// "code": http.StatusOK,
+		// "msg":  http.StatusText(http.StatusOK),
+		// "data": token})
+		return token
+	}
 
 }
 
 type wxOpenid struct {
-    Sessionkey string `json: "session_key"`
-    Openid     string `json: "openid"`
-    Errcode    int32  `json: "errcode"`
-    Errmsg     string `json: "errmsg"`
+	Sessionkey string `json: "session_key"`
+	Openid     string `json: "openid"`
+	Errcode    int32  `json: "errcode"`
+	Errmsg     string `json: "errmsg"`
 }
 
 //code换 openid
-func(wxc * WeixinCode) codetoopenid()(res * wxOpenid, e error) {
-    client: = &http.Client{}
+func (wxc *WeixinCode) codetoopenid() (res *wxOpenid, e error) {
+	client := &http.Client{}
 
-    // get url
-    wechat: = Lib.ServerConf.WeChat
-    url: = wechat.CodeUrl(wxc.Code)
-    //提交请求
-    reqest, err: = http.NewRequest("GET", url, nil)
+	// get url
+	wechat := Lib.ServerConf.WeChat
+	url := wechat.CodeUrl(wxc.Code)
+	//提交请求
+	reqest, err := http.NewRequest("GET", url, nil)
 
-    if err != nil {
-        panic(err)
-    }
+	if err != nil {
+		panic(err)
+	}
 
-    //处理返回结果
-    response, _: = client.Do(reqest)
-    //将结果定位到标准输出 也可以直接打印出来 或者定位到其他地方进行相应的处理
-    //body, _: = ioutil.ReadAll(response.Body)
-    defer response.Body.Close()
-    body, _: = ioutil.ReadAll(response.Body)
-    resp: = &wxOpenid{}
-    err = json.Unmarshal(body, resp)
-    return & wxOpenid{
-        resp.Sessionkey,
-        resp.Openid,
-        resp.Errcode,
-        resp.Errmsg}, err
+	//处理返回结果
+	response, _ := client.Do(reqest)
+	//将结果定位到标准输出也可以直接打印出来 或者定位到其他地方进行相应的处理
+	//body, _ := ioutil.ReadAll(response.Body)
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	resp := &wxOpenid{}
+	err = json.Unmarshal(body, resp)
+	return &wxOpenid{
+		resp.Sessionkey,
+		resp.Openid,
+		resp.Errcode,
+		resp.Errmsg}, err
 }
 
-func Wechat(ctx * gin.Context) {
-    //微信方式登录
-    weixincode: = WeixinCode{}
-    if err: = ctx.ShouldBindJSON(& weixincode); err != nil {
-        ctx.AbortWithStatusJSON(http.StatusBadRequest,
-                                gin.H{"code": http.StatusBadRequest, "msg": http.StatusText(http.StatusBadRequest)})
-    } else {
-        body, err: = weixincode.codetoopenid()
-        if err != nil {
-            ctx.AbortWithStatusJSON(http.StatusInternalServerError,
-                                    gin.H{"code": http.StatusInternalServerError, "msg": err.Error()})
-        } else {
-            if body.Errcode > 0 {
-                ctx.AbortWithStatusJSON(http.StatusOK,
-                                        gin.H{"code": body.Errcode, "msg": body.Errmsg})
-            } else {
-                userone: = &M.User{}
-                        err: = M.Users().Find(bson.M{"WxOpenId": body.Openid}).One(& userone)
-                if err != nil {
-                            if userone.WxOpenId == "" {
-                                userone.WxOpenId = body.Openid
-                                userone.Id = bson.NewObjectId()
-                                userone.CreateDateTime = time.Now()
-                                M.Users().Insert(userone)
-                            }
-                }
-                token: = generateToken(ctx, Lib.Payload{OpenId: body.Openid})
-                ctx.JSON(http.StatusOK, gin.H{
-                    "code":      http.StatusOK,
-                    "msg":       http.StatusText(http.StatusOK),
-                    "data":      token,
-                    "personmid": userone.PersonId})
-            }
-        }
-    }
+func Wechat(ctx *gin.Context) {
+	//微信方式登录
+	weixincode := WeixinCode{}
+	if err := ctx.ShouldBindJSON(&weixincode); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusBadRequest,
+			gin.H{"code": http.StatusBadRequest, "msg": http.StatusText(http.StatusBadRequest)})
+	} else {
+		body, err := weixincode.codetoopenid()
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError,
+				gin.H{"code": http.StatusInternalServerError, "msg": err.Error()})
+		} else {
+			if body.Errcode > 0 {
+				ctx.AbortWithStatusJSON(http.StatusOK,
+					gin.H{"code": body.Errcode, "msg": body.Errmsg})
+			} else {
+				userone := &M.User{}
+				err := M.Users().Find(bson.M{"WxOpenId": body.Openid}).One(&userone)
+				if err != nil {
+					if userone.WxOpenId == "" {
+						userone.WxOpenId = body.Openid
+						userone.Id = bson.NewObjectId()
+						userone.CreateDateTime = time.Now()
+						M.Users().Insert(userone)
+					}
+				}
+				token := generateToken(ctx, Lib.Payload{OpenId: body.Openid})
+				ctx.JSON(http.StatusOK, gin.H{
+					"code":      http.StatusOK,
+					"msg":       http.StatusText(http.StatusOK),
+					"data":      token,
+					"personmid": userone.PersonId})
+			}
+		}
+	}
 }
